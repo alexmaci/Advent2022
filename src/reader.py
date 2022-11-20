6b8f72c79a7cbe31061e4cce818302cebb7b18bf
Alejandro Vicente Macian <alexmaci96@gmail.com>
2022-11-20 09:33:31 +0100

    Cambios al lector

--- src/reader.py
+++ src/reader.py
@@ -1,5 +1,6 @@
 
 def readText(ruta):
     with open('./inputs/' + ruta + '.txt', 'r') as file:
-        data = file.read().replace('\n', ' ')
+        data = file.read()
+        data = data.splitlines()
         return data
